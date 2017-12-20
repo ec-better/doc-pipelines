bdf2167ab2bbd4b236dcaf9fb5464eb904884d68
fabricebrito <fabrice.brito@terradue.com>
2017-12-20 16:38:54 +0100

    udpate

--- source/conf.py
+++ source/conf.py
@@ -39,8 +39,7 @@ extensions = [
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
-    'sphinxcontrib.plantuml',
-    'githubpath'
+    'sphinxcontrib.plantuml'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
