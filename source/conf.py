23fa3db3b93ea13587da60e4f3e5d25255c59ffa
Fabrice Brito <fabrice.brito@terradue.com>
2018-06-18 10:26:11 +0200

    update

--- source/conf.py
+++ source/conf.py
@@ -43,6 +43,9 @@ extensions = [
     'nbsphinx'
 ]
 
+# nbsphinx
+exclude_patterns = ['_build', '**.ipynb_checkpoints']
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
